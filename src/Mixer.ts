90d243f9d08a32f87bcd0e9dd7d2bc8e32b9cf55
0b5vr <0b5vr@0b5vr.com>
2024-11-21 23:35:56 +0900

    feat: Implement Isolator EQ
    generate a 4th order Linkwitz-Riley FIR to achieve linear phase isolators
    
    I will address the view side in the next commit

--- src/Mixer.ts
+++ src/Mixer.ts
@@ -1,3 +1,4 @@
+import { MixerChannel, MixerChannelChangeEvent } from './MixerChannel';
 import { Analyser } from './Analyser';
 import { EventEmittable } from './utils/EventEmittable';
 import { LevelMeter } from './LevelMeter';
@@ -9,10 +10,14 @@ import { xfaderCurveTransition } from './xfaderCurveTransition';
 
 export type XFaderModeType = 'constantPower' | 'cut' | 'linear' | 'transition';
 
+interface MixerChangeEvent {
+  xfaderPos?: number;
+  channelA?: MixerChannelChangeEvent;
+  channelB?: MixerChannelChangeEvent;
+}
+
 interface MixerEvents {
-  changeVolumeA: { value: number };
-  changeVolumeB: { value: number };
-  changeXFader: { value: number };
+  change: MixerChangeEvent;
 }
 
 export class Mixer extends EventEmittable<MixerEvents> {
@@ -21,24 +26,14 @@ export class Mixer extends EventEmittable<MixerEvents> {
     return this.__audio;
   }
 
-  private __volumeA = 1.0;
-  public get volumeA(): number {
-    return this.__volumeA;
-  }
-  public set volumeA( value: number ) {
-    this.__volumeA = value;
-    this.__updateGains();
-    this.__emit( 'changeVolumeA', { value } );
+  private __channelA: MixerChannel;
+  public get channelA(): MixerChannel {
+    return this.__channelA;
   }
 
-  private __volumeB = 1.0;
-  public get volumeB(): number {
-    return this.__volumeB;
-  }
-  public set volumeB( value: number ) {
-    this.__volumeB = value;
-    this.__updateGains();
-    this.__emit( 'changeVolumeB', { value } );
+  private __channelB: MixerChannel;
+  public get channelB(): MixerChannel {
+    return this.__channelB;
   }
 
   private __xFaderPos = 0.5;
@@ -47,22 +42,20 @@ export class Mixer extends EventEmittable<MixerEvents> {
   }
   public set xFaderPos( value: number ) {
     this.__xFaderPos = value;
-    this.__updateGains();
-    this.__emit( 'changeXFader', { value } );
+    this.__updateXFaderGains();
+    this.__emit( 'change', { xfaderPos: value } );
   }
 
-  private __gainNodeA: GainNode;
-  private __gainNodeB: GainNode;
   private __gainXFaderA: GainNode;
   private __gainXFaderB: GainNode;
   private __gainNodeOut: GainNode;
 
   public get inputA(): AudioNode {
-    return this.__gainNodeA;
+    return this.__channelA.input;
   }
 
   public get inputB(): AudioNode {
-    return this.__gainNodeB;
+    return this.__channelB.input;
   }
 
   public get output(): AudioNode {
@@ -82,14 +75,14 @@ export class Mixer extends EventEmittable<MixerEvents> {
 
     this.__audio = audio;
 
-    this.__gainNodeA = audio.createGain();
-    this.__gainNodeB = audio.createGain();
+    this.__channelA = new MixerChannel( audio );
+    this.__channelB = new MixerChannel( audio );
     this.__gainXFaderA = audio.createGain();
     this.__gainXFaderB = audio.createGain();
     this.__gainNodeOut = audio.createGain();
 
-    this.__gainNodeA.connect( this.__gainXFaderA );
-    this.__gainNodeB.connect( this.__gainXFaderB );
+    this.__channelA.output.connect( this.__gainXFaderA );
+    this.__channelB.output.connect( this.__gainXFaderB );
     this.__gainXFaderA.connect( this.__gainNodeOut );
     this.__gainXFaderB.connect( this.__gainNodeOut );
 
@@ -97,8 +90,8 @@ export class Mixer extends EventEmittable<MixerEvents> {
     this.analyserInB = new Analyser( audio );
     this.analyserOut = new Analyser( audio );
 
-    this.__gainNodeA.connect( this.analyserInA.input );
-    this.__gainNodeB.connect( this.analyserInB.input );
+    this.__channelA.output.connect( this.analyserInA.input );
+    this.__channelB.output.connect( this.analyserInB.input );
     this.__gainNodeOut.connect( this.analyserOut.input );
 
     this.levelMeterInA = new LevelMeter( this.analyserInA );
@@ -106,8 +99,11 @@ export class Mixer extends EventEmittable<MixerEvents> {
     this.levelMeterOut = new LevelMeter( this.analyserOut );
 
     SETTINGSMAN.on( 'changeXFaderMode', () => {
-      this.__updateGains();
+      this.__updateXFaderGains();
     } );
+
+    this.__channelA.on( 'change', ( event ) => this.__emit( 'change', { channelA: event } ) );
+    this.__channelB.on( 'change', ( event ) => this.__emit( 'change', { channelB: event } ) );
   }
 
   public updateAnalyser( deltaTime: number ): void {
@@ -116,13 +112,10 @@ export class Mixer extends EventEmittable<MixerEvents> {
     this.analyserOut.update( deltaTime );
   }
 
-  private __updateGains(): void {
+  private __updateXFaderGains(): void {
     const [ a, b ] = this.__getXFaderValue();
 
     const time = this.__audio.currentTime + 0.005;
-
-    this.__gainNodeA.gain.linearRampToValueAtTime( this.__volumeA, time );
-    this.__gainNodeB.gain.linearRampToValueAtTime( this.__volumeB, time );
 
     this.__gainXFaderA.gain.linearRampToValueAtTime( a, time );
     this.__gainXFaderB.gain.linearRampToValueAtTime( b, time );
---
+++ src/MixerChannel.ts
@@ -0,0 +1,62 @@
+import { MixerEQ, MixerEQChangeEvent } from './MixerEQ';
+import { EventEmittable } from './utils/EventEmittable';
+
+export interface MixerChannelChangeEvent {
+  gain?: number;
+  eq?: MixerEQChangeEvent;
+}
+
+interface MixerChannelEvents {
+  change: MixerChannelChangeEvent;
+}
+
+export class MixerChannel extends EventEmittable<MixerChannelEvents> {
+  private __audio: AudioContext;
+  public get audio(): AudioContext {
+    return this.__audio;
+  }
+
+  private __gain = 1.0;
+  public get gain(): number {
+    return this.__gain;
+  }
+  public set gain( value: number ) {
+    this.__gain = value;
+
+    const time = this.__audio.currentTime + 0.005;
+    this.__gainNode.gain.linearRampToValueAtTime( this.__gain, time );
+
+    this.__emit( 'change', { gain: value } );
+  }
+
+  private __eq: MixerEQ;
+  public get eq(): MixerEQ {
+    return this.__eq;
+  }
+
+  private __gainNode: GainNode;
+  private __gainNodeOut: GainNode;
+
+  public get input(): AudioNode {
+    return this.__gainNode;
+  }
+
+  public get output(): AudioNode {
+    return this.__gainNodeOut;
+  }
+
+  public constructor( audio: AudioContext ) {
+    super();
+
+    this.__audio = audio;
+
+    this.__gainNode = audio.createGain();
+    this.__eq = new MixerEQ( audio );
+    this.__gainNodeOut = audio.createGain();
+
+    this.__eq.on( 'change', ( event ) => this.__emit( 'change', { eq: event } ) );
+
+    this.__gainNode.connect( this.__eq.input );
+    this.__eq.output.connect( this.__gainNodeOut );
+  }
+}
