0f7faf36a06e925a63ab82b7a0c99519fd8dc228
0b5vr <0b5vr@0b5vr.com>
2024-12-02 23:33:04 +0900

    deps: reinit eslint from scratch
    this bumps eslint from v8 to v9
    
    I'm tired of custom eslint config
    
    I will fix lint issues in the next commit

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import stylistic from '@stylistic/eslint-plugin';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,ts,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  stylistic.configs.customize({
+    semi: true,
+  }),
+];
