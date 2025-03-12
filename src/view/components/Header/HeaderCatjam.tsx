1352317a0826c7b0a173950e9de40ee273accdc7
0b5vr <0b5vr@0b5vr.com>
2025-03-12 21:00:19 +0900

    ui: fix catjam stalker text

--- src/view/components/Header/HeaderCatjam.tsx
+++ src/view/components/Header/HeaderCatjam.tsx
@@ -44,7 +44,7 @@ export function HeaderCatjam() {
       style={{
         backgroundPosition: `-${frame * 32}px 0`,
       }}
-      data-stalker="vibing"
+      data-stalker="live cat reaction"
     />
   );
 }
