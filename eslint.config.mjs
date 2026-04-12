8316295594209efd6b6417f41d3a5e08d7ac2bbd
0b5vr <0b5vr@0b5vr.com>
2026-04-12 19:55:13 +0900

    chore, deps: migrate config from new create-vite
    vite is 8.0.3 instead of 8.0.4, since it didn't compliant with min-release-age rule
    
    several existing deps might not be compatible with new deps, will fix in next commits

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,35 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import stylistic from '@stylistic/eslint-plugin';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    ignores: [
-      '**/dist/**/*',
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      stylistic.configs.customize({
+        semi: true,
+        braceStyle: '1tbs',
+        arrowParens: true,
+        quoteProps: 'consistent',
+      }),
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
     ],
-  },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat['jsx-runtime'],
-  stylistic.configs.customize({
-    semi: true,
-    braceStyle: '1tbs',
-    arrowParens: true,
-    quoteProps: 'consistent',
-  }),
-  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@stylistic/max-statements-per-line': ['error', { max: 2 }],
     },
   },
-];
+]);
